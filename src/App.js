7376b3a8f6d3aaac48f883f63420498e6f6b44a5
Anica Sutic <a.sutic@vega.local>
2019-07-16 16:09:19 +0200

    added header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage';
-import {Route} from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
 import ShopPage from './pages/ShopPage';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop' component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
