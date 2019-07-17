2c8aa2494954f5716bd2e9b8dd1cc6db9c402686
Anica Sutic <a.sutic@vega.local>
2019-07-17 16:20:08 +0200

    cart added item

--- src/components/CartDropdown/index.jsx
+++ src/components/CartDropdown/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './index.scss';
 import CustomButton from '../CustomButton';
+import {connect} from 'react-redux';
+import CartItem from '../CartItem';
 
-export default function CartDropdown() {
+function CartDropdown({cartItems}) {
     return (
         <div className='cart-dropdown'>
-            <div className="cart-items" />
+            <div className="cart-items">
+                {cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))}
+            </div>
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        cartItems: state.cart.cartItems
+    }
+}
+
+
+export default connect(mapStateToProps,null)(CartDropdown)
