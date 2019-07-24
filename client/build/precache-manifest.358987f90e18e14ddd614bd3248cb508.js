52150b3181c2d7099a64066b09361fc1f7d1f82b
Anica Sutic <a.sutic@vega.local>
2019-07-24 12:16:27 +0200

    added build]

---
+++ client/build/precache-manifest.358987f90e18e14ddd614bd3248cb508.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03093191383bb921c9dde55f887125ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53ad6a9ac1d362a3e094",
+    "url": "/static/css/3.bc591745.chunk.css"
+  },
+  {
+    "revision": "4c88755385ab47534915",
+    "url": "/static/css/4.774ec2bd.chunk.css"
+  },
+  {
+    "revision": "e9e3a524d5cdd4a700eb",
+    "url": "/static/css/main.1ffdb826.chunk.css"
+  },
+  {
+    "revision": "b416e511b277647039bc",
+    "url": "/static/js/2.78bd6432.chunk.js"
+  },
+  {
+    "revision": "53ad6a9ac1d362a3e094",
+    "url": "/static/js/3.aefddb15.chunk.js"
+  },
+  {
+    "revision": "4c88755385ab47534915",
+    "url": "/static/js/4.0f6536bc.chunk.js"
+  },
+  {
+    "revision": "e9e3a524d5cdd4a700eb",
+    "url": "/static/js/main.10898ee6.chunk.js"
+  },
+  {
+    "revision": "c154fbb7ca62e21da4b2",
+    "url": "/static/js/runtime~main.ef579133.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
