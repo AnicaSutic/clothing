be75883622e2af19fa504c8a19dd5b96bc574b04
Anica Sutic <a.sutic@vega.local>
2019-08-21 11:29:54 +0200

    test

--- client/src/components/MenuItem/index.jsx
+++ client/src/components/MenuItem/index.jsx
@@ -8,7 +8,7 @@ function MenuItem({ title, imageUrl, size, history, linkUrl ,match}) {
         <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">Shop now</span>
+        <span className="subtitle">SHOP</span>
       </div>
     </div>
   );
